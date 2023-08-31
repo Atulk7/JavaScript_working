bef5f688f1fbc5b21594cc680fd56658463a90e5
Atulk7 <atulkevat7@gmail.com>
2023-08-31 11:12:07 +0000

    memory concept added

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -11,7 +11,7 @@ let userEmail;
 const id = Symbol("123");
 const anotherId = Symbol("123");
 
-console.log(id === anotherId); //false
+// console.log(id === anotherId); //false
 
 // const bigNumber = 4237423482374423n;
 
@@ -30,6 +30,30 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof outsideTemp );//object
-console.log(typeof anotherId );//symbol
-console.log(typeof myFunction );//function object
+// console.log(typeof outsideTemp );//object
+// console.log(typeof anotherId );//symbol
+// console.log(typeof myFunction );//function object
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++
+//stack(primitive)-> always a get a copy, heap(non-primitive)-> always get a memory address
+
+let myYoutubeName = "hiteshchoudharydotcom";
+
+let anotherName = myYoutubeName;
+anotherName = "chaiaurcode";
+
+// console.log(anotherName);//chaiaurcode
+// console.log(myYoutubeName);//hiteshchoudharydotcom
+
+let userOne = {
+  email: "user@google.com",
+  upi: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "atul@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
