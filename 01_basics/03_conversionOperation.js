71dc373831f782b249c1efa84bfab65a2f20402a
Atulk7 <atulkevat7@gmail.com>
2023-08-31 10:36:33 +0000

    comparisions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,6 +20,36 @@ let booleanIsLoogedIn = Boolean(isLoggedIn);
 let someNumber =33;
 
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+//********************* Operations **************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Hello";
+let str2 = " Atul";
+let str3 = str1 + str2;
+// console.log(str3);//Hello Atul
+
+// console.log("1" + 2  + 2);//122 
+// console.log(1 + 2  + "2");//32 
+
+// console.log(+true);//1
+
+
+
+
+
 
 
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 4);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);//false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true -> the reason is that an equality check == and comparisions > < >= <= work differently. Comparisions convert null to a number, treating it as 0 That's why (3) null >= 0 is tru and (1) null > 0 is false
+
+
+// console.log(undefined == 0);//false
+// console.log(undefined < 0);//false
+// console.log(undefined > 0);//false
+
+
+// === strict check
+console.log("2" === 2);
