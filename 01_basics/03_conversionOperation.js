92c1d6fe5b8921beb307472509a84c3a2f815272
Atulk7 <atulkevat7@gmail.com>
2023-08-31 10:03:52 +0000

    conversions of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33";//"Hitesh";-Nan //true;-1//undefined;-NaN//null;-0//"33abc";-Nan//33;-33
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);//convert into number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);//NaN
+
+//"33" => 33
+//"33avf" => NaN
+//"true" => 1; false => 0
+
+
+let isLoggedIn =  1//11;-true//"Atul";-true//"";-false//1-true;
+
+let booleanIsLoogedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoogedIn);
+
+
+let someNumber =33;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+
+
