5eb9f939559029c9429a43fbf5ee9a06ceaa59fb
Atulk7 <atulkevat7@gmail.com>
2023-09-06 06:29:11 +0000

    iife function working

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -3,12 +3,21 @@
 //NOte -> to avoid global scope polution we use iife function expression
 
 (function chai(){
+    //named iife
     console.log(`DB CONNECTED`);
-})();//MUST SEMICOLLON
+})();//MUST SEMICOLLON :- so that iife function knowing when to stop 
 
-//iife with arrow
-( () => {
-    console.log(`DB CONNECTED TWO`);
-} )()
+// //iife with arrow
+// ( () => {
+    // unnamed iife
+//     console.log(`DB CONNECTED TWO`);
+// } )()
 
-// chai();//not need to call auto call function 
+// chai();//not need to call auto call function. 
+
+
+
+//when we want to take parameter into iife function and look how to pass argument into a iife function
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('atul-kevat')
